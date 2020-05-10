0ec3432fc22dbeefc55271cd2d587a9ec5cd453d
Alvaro Serna <alsern01@ucm.es>
2020-05-10 17:54:22 +0200

    ejercicio6 a falta de algunas cosas

--- practica2.1/ejercicio6/ejercicio6.cc
+++ practica2.1/ejercicio6/ejercicio6.cc
@@ -4,44 +4,108 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <time.h>
+
 #include <iostream>
+
 #include <thread>
+#include <mutex>
+#include <condition_variable>
+
 #include <vector>
 
-void haz_mensaje(int sd, int id)
+// Mutex para la seccion critica
+static std::mutex MUTEX;
+static std::condition_variable V_COND;
+static bool END = false;
+
+class Mensaje
 {
-    // ---------------------------------------------------------------------- //
-    // RECEPCIÓN MENSAJE DE CLIENTE //
-    // ---------------------------------------------------------------------- //
-    char buffer[80];
-    char host[NI_MAXHOST];
-    char service[NI_MAXSERV];
+  private:
+    int sd_;
+    bool up_ = true;
 
-    struct sockaddr client_addr;
-    socklen_t client_len = sizeof(struct sockaddr);
+  public:
+    Mensaje(int sd) : sd_(sd){};
+    ~Mensaje(){};
 
-    ssize_t bytes = recvfrom(sd, buffer, 79 * sizeof(char), 0, &client_addr,
-                             &client_len);
-
-    if (bytes == -1)
+    // metodo para gestionar mensajes
+    void haz_mensaje()
     {
-        std::cerr << "recvfrom: " << std::endl;
-        return;
+        // ---------------------------------------------------------------------- //
+        // RECEPCIÓN MENSAJE DE CLIENTE //
+        // ---------------------------------------------------------------------- //
+        char buffer[80];
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+
+        struct sockaddr client_addr;
+        socklen_t client_len = sizeof(struct sockaddr);
+
+        while (up_)
+        {
+            memset(&buffer, 0, sizeof(buffer));
+            ssize_t bytes = recvfrom(sd_, buffer, 79 * sizeof(char), 0, &client_addr,
+                                     &client_len);
+
+            if (bytes == -1)
+            {
+                std::cerr << "recvfrom: " << std::endl;
+                return;
+            }
+
+            int err = getnameinfo(&client_addr, client_len, host, NI_MAXHOST, service,
+                                  NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+
+            std::cout << bytes << " bytes de  " << host << ":" << service << std::endl;
+
+            if (err != 0)
+            {
+                std::cerr << "getnameinfo: " << std::endl;
+                return;
+            }
+
+            time_t rawtime;
+            size_t len;
+            struct tm *info;
+            char buf[sizeof(buffer)];
+            memset(&buf, 0, sizeof(buf));
+
+            time(&rawtime);
+            info = localtime(&rawtime);
+
+            if (buffer[0] == 't')
+            {
+                strftime(buf, 80, "%I:%M:%S %p", info);
+                sendto(sd_, buf, strlen(buf), 0, &client_addr, client_len);
+            }
+
+            else if (buffer[0] == 'd')
+            {
+                len = strftime(buf, 80, "%F", info);
+                sendto(sd_, buf, strlen(buf), 0, &client_addr, client_len);
+            }
+
+            else if (buffer[0] == 'q')
+            {
+                std::cout << "Saliendo..." << std::endl;
+                up_ = false;
+                END = true;
+                std::unique_lock<std::mutex> lock(MUTEX);
+                V_COND.notify_one();
+            }
+
+            else
+            {
+                std::cout << "Comando no soportado " << buffer[0] << std::endl;
+            }
+
+            std::cout << "THREAD: " << std::this_thread::get_id() << std::endl;
+            std::cout << "--------------------------" << std::endl;
+            sleep(10);
+        }
     }
-
-    getnameinfo(&client_addr, client_len, host, NI_MAXHOST, service,
-                NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-
-    std::cout << "THREAD: " << id << " IP: " << host << " PUERTO: " << service
-              << "MENSAJE: " << buffer << std::endl;
-
-    sleep(2);
-
-    // ---------------------------------------------------------------------- //
-    // RESPUESTA AL CLIENTE //
-    // ---------------------------------------------------------------------- //
-    sendto(sd, buffer, bytes, 0, &client_addr, client_len);
-}
+};
 
 int main(int argc, char **argv)
 {
@@ -84,8 +148,15 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < 5; ++i)
     {
-        pool.push_back(std::thread(haz_mensaje, sd, i));
+        pool.push_back(std::thread([&]() {
+            Mensaje msg(sd);
+            msg.haz_mensaje();
+        }));
     }
+
+    // MUTEX
+    std::unique_lock<std::mutex> lock(MUTEX);
+    V_COND.wait(lock, [&]() { return END; });
 
     for (auto &t : pool)
     {
